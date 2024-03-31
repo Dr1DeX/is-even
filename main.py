5560cc4dae4e1f40ff70461f473c06cb2c718387
nesterov-art <nesterov-art@yandex-team.ru>
2024-03-31 11:54:07 +0300

    Add main algo, tests, main workflow

---
+++ main.py
@@ -0,0 +1,6 @@
+def is_even(value):
+    return value % 2 == 0
+
+
+def is_even_mod(value):
+    return value & 1 == 0
---
+++ tests.py
@@ -0,0 +1,25 @@
+from main import is_even, is_even_mod
+import unittest
+import time
+
+
+class TestCorrectRun(unittest.TestCase):
+    def test_is_even(self):
+        corner_case = [
+            (2, True),
+            (4, True),
+            (0, True),
+            ('', False),
+            (3, False),
+        ]
+
+        for params, expected in corner_case:
+            with self.subTest(params=params, expected=expected):
+                actual = is_even(params)
+                actual_mod = is_even_mod(params)
+                self.assertEqual(actual, expected)
+                self.assertEqual(actual_mod, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
